16c2d5258c2a7c4968789a23f5f139a5c47860c4
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-23 21:30:33 -0500

    so far

--- swatch/mouse.go
+++ swatch/mouse.go
@@ -1,11 +1,12 @@
 // Mouse clicking and rendering
 package swatch
 
-import "fyne.io/fyne/driver/desktop"
+import "fyne.io/fyne/v2/driver/desktop"
 
 func (swatch *Swatch) MouseDown(ev *desktop.MouseEvent) {
 	swatch.clickHandler(swatch)
 	swatch.Selected = true
 	swatch.Refresh()
 }
-func (swatch *Swatch) MouseUp(ev *desktop.MouseEvent)   {}
+func (swatch *Swatch) MouseUp(ev *desktop.MouseEvent) {
+}
